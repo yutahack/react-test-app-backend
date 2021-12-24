c957f9882acdc018756b5927713fcc546ed0028f
yutahack <yutahack@gmail.com>
2021-12-24 19:56:45 +0900

    pg 수저

--- src/pg/pg.js
+++ src/pg/pg.js
@@ -1,2 +1,10 @@
 const { Client } = require("pg");
 const Query = require("pg").Query;
+
+var client = new Client({
+    user: "user",
+    host: "localhost",
+    database: "postgres",
+    password: "password",
+    port: 5432,
+});
