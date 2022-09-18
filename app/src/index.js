0d8e95a32900be745eab59b62571251c50295503
yutahack <yutahack@gmail.com>
2022-09-18 23:49:49 +0900

    불필요 파일/코드 삭제

--- app/src/index.js
+++ app/src/index.js
@@ -7,60 +7,7 @@ const schema = require("./schema");
 const resolver = require("./resolver");
 const pg = require("./pg/pg");
 
-// const port = config.GQL_API_BOOKS_PORT;
-// const name = config.GQL_API_BOOKS_NAME;
 const port = 4000;
-
-const books = [
-    {
-        isbn: "978-111",
-        title: "Web API Design Handbook",
-        authors: ["George Reese", "Christian Reilly"],
-        price: 4.99,
-    },
-    {
-        isbn: "978-222",
-        title: "React Programming Basics",
-        authors: ["Eric Freeman", "Elisabeth Robson"],
-        price: 38.97,
-    },
-    {
-        isbn: "978-333",
-        title: "GraphQL in Depth",
-        authors: ["Matthias Biehl"],
-        price: 9.99,
-    },
-];
-
-// const BookQuery = (args) => {
-//     const isbn = args.isbn;
-//     return books.filter((Book) => Book.isbn === isbn)[0];
-// };
-
-// const BooksQuery = (args) => {
-//     if (args.title) {
-//         const title = args.title;
-//         return books.filter((Book) => Book.title.match(new RegExp(title)));
-//     }
-//     return books;
-// };
-
-// const resolvers = {
-//     events: async (_, context) => {
-//         const { db } = await context();
-//         return db.collection("events").find().toArray();
-//     },
-//     event: async ({ id }, context) => {
-//         console.log("EVENT!!!!!!!!!!!!");
-//         const { db } = await context();
-//         return db.collection("events").findOne({ id });
-//     },
-
-//     Book: async () => {
-//         console.log("bbbbbbok!");
-//         return;
-//     },
-// };
 
 app.use(cors());
 app.use(
